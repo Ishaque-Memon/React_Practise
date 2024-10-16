045d3a1d142f3572d6a7a514c934bec15d79a675
Muhammad Ishaque <haseebmeymon123@gmail.com>
2024-10-17 00:12:30 +0500

    creating react app

--- module1.mjs
+++
@@ -1,8 +0,0 @@
-import name, {a, b, d} from './module2.mjs'
-
-console.log(name);
-console.log(a);
-console.log(b);
-console.log(d);
-
-
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Haseeb"
-const b = "Shaheer"
-const c = "Ali"
-const d = "Ahsan"
-
-export default c;
-export {a};
-export {b};
-export {d};
